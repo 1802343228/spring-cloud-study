599f2270744aa30a739db494a8c6649695324897
crq <2631315464@qq.com>
2020-09-22 15:38:34 +0800

    spring cloud alibaba起步

---
+++ share-app/content-center/src/main/java/com/soft1851/content/ContentApplication.java
@@ -0,0 +1,13 @@
+package com.soft1851.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+
+}
